4b0ed6b6ecc16a5c9d97aba54736155701bcd552
Andrey-VU <avustjuzhanin@yandex.ru>
2023-07-07 15:10:41 +0300

    Refactor: завершить перенос ItemRequestController в gateway

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -23,38 +23,38 @@ public class ItemRequestController {
 
     @PostMapping
     ItemRequestDto addRequest(@RequestHeader("X-Sharer-User-Id") Long requesterId,
-                              @Valid @RequestBody ItemRequestDto itemRequestDto) {
-        log.info("Add new request: {} by user id {} - Started", itemRequestDto, requesterId);
+                              @RequestBody ItemRequestDto itemRequestDto) {
+        log.info("SERVER: Add new request: {} by user id {} - Started", itemRequestDto, requesterId);
         ItemRequestDto itemRequestDtoFromRepo = itemRequestService.addNewItemRequest(requesterId, itemRequestDto);
-        log.info("Add new request: {} - Finished", itemRequestDtoFromRepo);
+        log.info("SERVER: Add new request: {} - Finished", itemRequestDtoFromRepo);
         return itemRequestDtoFromRepo;
     }
 
     @GetMapping
     List<ItemRequestDto> getItemRequests(@RequestHeader("X-Sharer-User-Id") Long requesterId) {
-        log.info("Get requests for user id: {} - Started", requesterId);
+        log.info("SERVER: Get requests for user id: {} - Started", requesterId);
         List<ItemRequestDto> listOfRequestsDto = itemRequestService.getItemRequests(requesterId);
-        log.info("Size of founded List of requests is {} - Finished", listOfRequestsDto.size());
+        log.info("SERVER: Size of founded List of requests is {} - Finished", listOfRequestsDto.size());
         return listOfRequestsDto;
     }
 
     @GetMapping("/all")
     List<ItemRequestDto> getAllItemRequests(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                            @Valid @RequestParam(required = false, defaultValue = "0") @Min(0) Integer from,
-                                            @Valid @RequestParam(required = false, defaultValue = "20") @Min(1) Integer size) {
+                                            @RequestParam(required = false, defaultValue = "0") @Min(0) Integer from,
+                                            @RequestParam(required = false, defaultValue = "20") @Min(1) Integer size) {
 
-        log.info("Get All requests - Started");
+        log.info("SERVER: Get All requests - Started");
         List<ItemRequestDto> listOfRequestsDto = itemRequestService.getAllItemRequests(userId, from, size);
-        log.info("Size of founded List of requests is {} - Finished", listOfRequestsDto.size());
+        log.info("SERVER: Size of founded List of requests is {} - Finished", listOfRequestsDto.size());
         return listOfRequestsDto;
     }
 
     @GetMapping("/{requestId}")
     ItemRequestDto getItemRequest(@RequestHeader("X-Sharer-User-Id") Long userId,
                                   @PathVariable Long requestId) {
-        log.info("Get request id: {} - Started", requestId);
+        log.info("SERVER: Get request id: {} - Started", requestId);
         ItemRequestDto requestItemDto = itemRequestService.getItemRequest(userId, requestId);
-        log.info("Request id {} was found - Finished", requestId);
+        log.info("SERVER: Request id {} was found - Finished", requestId);
 
         return requestItemDto;
     }
